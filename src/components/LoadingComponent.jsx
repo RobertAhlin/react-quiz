52d2d4ad83ec3ad27e18cc47d3511ee61ec73eca
Robert Ahlin <rob@ahlin.org>
2024-02-28 10:08:55 +0100

    Styled LoadingComponent

--- src/components/LoadingComponent.jsx
+++ src/components/LoadingComponent.jsx
@@ -1,9 +1,30 @@
 // LoadingComponent.jsx
 
-import React from 'react';
+import styled from 'styled-components';
+
+// Styled component for the loading spinner
+const Spinner = styled.div`
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  border-left-color: #007bff;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: spin 1s linear infinite;
+
+  @keyframes spin {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
 
 const LoadingComponent = () => {
-  return <div>Loading...</div>;
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+      <Spinner />
+      <p>Loading...</p>
+    </div>
+  );
 };
 
 export default LoadingComponent;
